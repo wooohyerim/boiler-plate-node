868beb90e6a8a6642cbae120a9c9931daafc2b0f
woohyerim <hyerim3414@gmail.com>
2024-02-29 18:45:08 +0900

    feat : bcrypt 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +31,23 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  let user = this;
+  //비밀번호 암호화 시킨다.
+  if (user.isModified("password")) {
+    //비밀번호가 변경됐을시에만 실행하도록
+    bcrypt.genSalt(saltRound, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
