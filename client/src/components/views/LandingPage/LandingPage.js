03430f00cc3295f236b7b11b87f25b31be9f99a4
woohyerim <hyerim3414@gmail.com>
2024-03-07 20:23:58 +0900

    ADD : proxy 세팅 및 server 통신 확인

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const LandingPage = () => {
+  useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h1>LandingPage</h1>
