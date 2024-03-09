e52f939efbca120c6b6ddbe0202ddcabbf78f95e
woohyerim <hyerim3414@gmail.com>
2024-03-09 14:35:48 +0900

    ADD : redux setting

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import { thunk as ReduxThunk } from "redux-thunk";
+import Reducer from "./_reducers";
+import "./index.css";
 // import "antd/dist/antd.css";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </>
+  </Provider>
 );
